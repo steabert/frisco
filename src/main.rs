23477dead927cbd1ea8c969c00e54adc0d3e3f4b
steabert <steabert@users.noreply.github.com>
2020-04-13 20:55:10 +0200

    chore: refactor into simpler big blob

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,59 @@
 extern crate dns_parser;
+extern crate httparse;
 
+use std::collections;
 use std::net;
 
-const MDNS_MULTICAST_DST: &str = "224.0.0.251:5353";
-const SSDP_MULTICAST_DST: &str = "239.255.255.250:1900";
+/// Print list of IP, name of discovered mDNS/SSDP services.
+fn main() {
+    // Store discovered addresses so we can skip them.
+    let mut address_store = collections::HashSet::new();
 
-/// Create a pair of sender/receiver sockets
-/// Note that we're not joining the multicast group,
-/// as we'll only listen for unicast responses to our question.
-fn socket_pair() -> (net::UdpSocket, net::UdpSocket) {
+    // Set up sending an receiving sockets.
     let host_addr = net::Ipv4Addr::new(0, 0, 0, 0);
-
     let sender = net::UdpSocket::bind(net::SocketAddrV4::new(host_addr, 0)).unwrap();
     let receiver = sender.try_clone().unwrap();
-
-    (sender, receiver)
-}
-
-fn main() {
-    println!("prototype service discovery");
 
     //
     // mDNS discovery
     //
-    let (mdns_sender, mdns_receiver) = socket_pair();
 
-    // listen for mDNS unicast answers
-    let mdns_handle = std::thread::spawn(move || {
+    // listen for mDNS unicast answers / SSDP responses
+    let receiver_handle = std::thread::spawn(move || {
         let mut buffer = [0; 4096];
         loop {
-            match mdns_receiver.recv_from(&mut buffer) {
+            match receiver.recv_from(&mut buffer) {
                 Ok((n_bytes, origin)) => {
-                    let packet = dns_parser::Packet::parse(&buffer[0..n_bytes]).unwrap();
-                    for answer in packet.answers {
-                        if let dns_parser::rdata::RData::PTR(dns_parser::rdata::Ptr(name)) =
-                            answer.data
-                        {
-                            println!("{} {:?}", origin.ip(), name.to_string());
+                    if address_store.contains(&origin) {
+                        continue;
+                    }
+                    address_store.insert(origin);
+                    if &buffer[0..4] == "HTTP".as_bytes() {
+                        // Assume it's an SSDP response.
+                        let mut headers = [httparse::EMPTY_HEADER; 32];
+                        let mut response = httparse::Response::new(&mut headers);
+                        if let Ok(_) = response.parse(&buffer[0..n_bytes]) {
+                            for header in response.headers {
+                                if header.name.to_ascii_lowercase() == "server" {
+                                    println!(
+                                        "{} {}",
+                                        origin.ip(),
+                                        std::str::from_utf8(header.value).unwrap()
+                                    );
+                                }
+                            }
                         }
+                    } else {
+                        // Assume it's a mDNS response.
+                        if let Ok(packet) = dns_parser::Packet::parse(&buffer[0..n_bytes]) {
+                            for answer in packet.answers {
+                                if let dns_parser::rdata::RData::PTR(dns_parser::rdata::Ptr(name)) =
+                                    answer.data
+                                {
+                                    println!("{} {}", origin.ip(), name.to_string());
+                                }
+                            }
+                        };
                     }
                 }
                 Err(msg) => println!("noooo! {}", msg),
@@ -54,41 +70,16 @@ fn main() {
         dns_parser::QueryClass::IN,
     );
     let packet_data = builder.build().unwrap();
-    mdns_sender
-        .send_to(&packet_data, MDNS_MULTICAST_DST)
-        .unwrap();
-
-    //
-    // SSDP discovery
-    //
-    let (ssdp_sender, ssdp_receiver) = socket_pair();
-
-    // listen for SDP reponses
-    let ssdp_handle = std::thread::spawn(move || {
-        let mut buffer = [0; 4096];
-        loop {
-            match ssdp_receiver.recv_from(&mut buffer) {
-                Ok((n_bytes, origin)) => {
-                    println!(
-                        "{} {:?}",
-                        origin.ip(),
-                        std::str::from_utf8(&buffer[0..n_bytes])
-                    );
-                }
-                Err(msg) => println!("noooo! {}", msg),
-            }; // blocking
-        }
-    });
+    sender.send_to(&packet_data, "224.0.0.251:5353").unwrap();
 
     // send SSDP M-SEARCH
     let m_search =
         "M-SEARCH * HTTP/1.1\rHost:239.255.255.250:1900\rMan:\"ssdp:discover\"\rST: ssdp:all\rMX: 1\r\n\r\n";
-    ssdp_sender
-        .send_to(m_search.as_bytes(), SSDP_MULTICAST_DST)
+    sender
+        .send_to(m_search.as_bytes(), "239.255.255.250:1900")
         .unwrap();
 
-    println!("listening...");
-
-    mdns_handle.join().unwrap();
-    ssdp_handle.join().unwrap();
+    // Gather responses
+    println!("listening for mDNS/SSDP replies...");
+    receiver_handle.join().unwrap();
 }
