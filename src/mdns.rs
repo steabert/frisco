810a09e80f9cf2c57f2e919f7a4b1495ecf2e972
steabert <steabert@users.noreply.github.com>
2020-08-26 08:09:30 +0200

    chore: improve logging formats

--- src/mdns.rs
+++ src/mdns.rs
@@ -1,31 +1,28 @@
 use async_std::net::{
-    IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6,
-    UdpSocket,
+    IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6, UdpSocket,
 };
 
+use std::error::Error;
 use std::sync;
 
 const PROTOCOL: &str = "mDNS";
 
 macro_rules! log_err {
-    ($s:expr, $($e:expr),*) => {
-        eprintln!(concat!("[{}]: ", $s), PROTOCOL, $($e),*);
+    ($e:expr) => {
+        eprintln!("[error]: {}: {}: {}", PROTOCOL, $e, $e.source().unwrap());
     };
 }
 
 macro_rules! log_fmt {
-    ($s:expr, $($e:expr),*) => {
-        format!(concat!("[{}]: ", $s), PROTOCOL, $($e),*);
+    ($ip:expr, $s:expr) => {
+        format!("{:16} {:8} {}", $ip, PROTOCOL, $s);
     };
 }
 
 // Get the mDNS socket address matching an IPv4/v6 address.
 fn multicast_socket_addr(ip_addr: IpAddr) -> SocketAddr {
     match ip_addr {
-        IpAddr::V4(_) => SocketAddr::V4(SocketAddrV4::new(
-            Ipv4Addr::new(224, 0, 0, 251),
-            5353,
-        )),
+        IpAddr::V4(_) => SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(224, 0, 0, 251), 5353)),
         IpAddr::V6(_) => SocketAddr::V6(SocketAddrV6::new(
             Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0xfb),
             5353,
@@ -49,9 +46,7 @@ fn build_packet() -> Vec<u8> {
 fn parse_response(data: &[u8]) -> Option<String> {
     if let Ok(packet) = dns_parser::Packet::parse(data) {
         for answer in packet.answers.first() {
-            if let dns_parser::rdata::RData::PTR(dns_parser::rdata::Ptr(name)) =
-                answer.data
-            {
+            if let dns_parser::rdata::RData::PTR(dns_parser::rdata::Ptr(name)) = answer.data {
                 return Some(name.to_string());
             }
         }
@@ -59,50 +54,40 @@ fn parse_response(data: &[u8]) -> Option<String> {
     return None;
 }
 
-pub async fn scan(
-    ip_addr: IpAddr,
-    scope: u32,
-    channel: sync::mpsc::Sender<String>,
-) {
+pub async fn scan(ip_addr: IpAddr, scope: u32, channel: sync::mpsc::Sender<String>) {
     let socket_addr = match ip_addr {
         IpAddr::V4(ipv4) => SocketAddr::V4(SocketAddrV4::new(ipv4, 0)),
-        IpAddr::V6(ipv6) => {
-            SocketAddr::V6(SocketAddrV6::new(ipv6, 0, 0, scope))
-        }
+        IpAddr::V6(ipv6) => SocketAddr::V6(SocketAddrV6::new(ipv6, 0, 0, scope)),
     };
 
     let socket = match UdpSocket::bind(socket_addr).await {
         Ok(socket) => socket,
-        Err(msg) => {
-            return log_err!("failed to bind {}: {}", socket_addr, msg);
+        Err(err) => {
+            return log_err!(err);
         }
     };
 
     let multicast_addr = multicast_socket_addr(ip_addr);
     let packet = build_packet();
-    if let Err(msg) = socket.send_to(&packet, &multicast_addr).await {
-        return log_err!("failed to send to {}: {}", multicast_addr, msg);
+    if let Err(err) = socket.send_to(&packet, &multicast_addr).await {
+        return log_err!(err);
     };
 
     let mut buf = [0_u8; 4096];
     loop {
         let (n_bytes, origin_addr) = match socket.recv_from(&mut buf).await {
             Ok(rsp) => rsp,
-            Err(msg) => {
-                return log_err!(
-                    "failed to receive on {}: {}",
-                    socket_addr,
-                    msg
-                );
+            Err(err) => {
+                return log_err!(err);
             }
         };
 
         let src_ip = origin_addr.ip();
         let data = &buf[0..n_bytes];
         if let Some(name) = parse_response(data) {
-            let log_msg = log_fmt!("{} {}", src_ip, name.to_string());
-            if let Err(msg) = channel.send(log_msg) {
-                return log_err!("logging failed: {}", msg);
+            let log_msg = log_fmt!(src_ip, name.to_string());
+            if let Err(err) = channel.send(log_msg) {
+                return log_err!(err);
             }
         }
     }
--- src/ssdp.rs
+++ src/ssdp.rs
@@ -1,31 +1,28 @@
 use async_std::net::{
-    IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6,
-    UdpSocket,
+    IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6, UdpSocket,
 };
 
+use std::error::Error;
 use std::sync;
 
 const PROTOCOL: &str = "SSDP";
 
 macro_rules! log_err {
-    ($s:expr, $($e:expr),*) => {
-        eprintln!(concat!("[{}]: ", $s), PROTOCOL, $($e),*);
+    ($e:expr) => {
+        eprintln!("[error]: {}: {}: {}", PROTOCOL, $e, $e.source().unwrap());
     };
 }
 
 macro_rules! log_fmt {
-    ($s:expr, $($e:expr),*) => {
-        format!(concat!("[{}]: ", $s), PROTOCOL, $($e),*);
+    ($ip:expr, $s:expr) => {
+        format!("{:16} {:8} {}", $ip, PROTOCOL, $s);
     };
 }
 
 // Get the SSDP socket address matching an IPv4/v6 address.
 fn multicast_socket_addr(ip_addr: IpAddr) -> SocketAddr {
     match ip_addr {
-        IpAddr::V4(_) => SocketAddr::V4(SocketAddrV4::new(
-            Ipv4Addr::new(239, 255, 255, 250),
-            1900,
-        )),
+        IpAddr::V4(_) => SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(239, 255, 255, 250), 1900)),
         IpAddr::V6(_) => SocketAddr::V6(SocketAddrV6::new(
             Ipv6Addr::new(0xff0e, 0, 0, 0, 0, 0, 0, 0xc),
             1900,
@@ -55,59 +52,47 @@ fn parse_response(data: &[u8]) -> Option<String> {
     if let Ok(_) = response.parse(data) {
         for header in response.headers {
             if header.name.to_ascii_lowercase() == "server" {
-                return Some(
-                    std::str::from_utf8(header.value).unwrap().to_string(),
-                );
+                return Some(std::str::from_utf8(header.value).unwrap().to_string());
             }
         }
     }
     return None;
 }
 
-pub async fn scan(
-    ip_addr: IpAddr,
-    scope: u32,
-    channel: sync::mpsc::Sender<String>,
-) {
+pub async fn scan(ip_addr: IpAddr, scope: u32, channel: sync::mpsc::Sender<String>) {
     let socket_addr = match ip_addr {
         IpAddr::V4(ipv4) => SocketAddr::V4(SocketAddrV4::new(ipv4, 0)),
-        IpAddr::V6(ipv6) => {
-            SocketAddr::V6(SocketAddrV6::new(ipv6, 0, 0, scope))
-        }
+        IpAddr::V6(ipv6) => SocketAddr::V6(SocketAddrV6::new(ipv6, 0, 0, scope)),
     };
 
     let socket = match UdpSocket::bind(socket_addr).await {
         Ok(socket) => socket,
-        Err(msg) => {
-            return log_err!("failed to bind {}: {}", socket_addr, msg);
+        Err(err) => {
+            return log_err!(err);
         }
     };
 
     let multicast_addr = multicast_socket_addr(ip_addr);
     let packet = build_packet(&multicast_addr);
-    if let Err(msg) = socket.send_to(&packet, &multicast_addr).await {
-        return log_err!("failed to send to {}: {}", multicast_addr, msg);
+    if let Err(err) = socket.send_to(&packet, &multicast_addr).await {
+        log_err!(err)
     }
 
     let mut buf = [0_u8; 4096];
     loop {
         let (n_bytes, origin_addr) = match socket.recv_from(&mut buf).await {
             Ok(rsp) => rsp,
-            Err(msg) => {
-                return log_err!(
-                    "failed to receive on {}: {}",
-                    socket_addr,
-                    msg
-                );
+            Err(err) => {
+                return log_err!(err);
             }
         };
 
         let src_ip = origin_addr.ip();
         let data = &buf[0..n_bytes];
         if let Some(name) = parse_response(data) {
-            let log_msg = log_fmt!("{} {}", src_ip, name.to_string());
-            if let Err(msg) = channel.send(log_msg) {
-                return log_err!("logging failed: {}", msg);
+            let log_msg = log_fmt!(src_ip, name.to_string());
+            if let Err(err) = channel.send(log_msg) {
+                return log_err!(err);
             }
         }
     }
